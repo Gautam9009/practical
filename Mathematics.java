659d46145e90d8b53c055b9c32733d1b56de0e77
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-01-30 21:49:15 +0530

    Add files via upload

---
+++ Mathematics.java
@@ -0,0 +1,18 @@
+class Mathematics{
+public static void main (String args[]){
+
+int a=50;
+
+a+=10;
+System.out.println(a);
+
+a-=20;
+System.out.println(a);
+
+a*=2;
+System.out.println(a);
+
+a/=8;
+System.out.println(a);
+}
+}
