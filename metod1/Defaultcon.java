03e6a011d79d039c9f01236047b66e5fe15be31a
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-02-07 21:28:28 +0530

    Add files via upload

---
+++ metod1/Defaultcon.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class Defaultcon {
+	Defaultcon(){
+		System.out.println("Default constructor is created");
+	}
+	public static void main(String[] args) {
+		Defaultcon Defaultcon=new Defaultcon();
+	}
+}
