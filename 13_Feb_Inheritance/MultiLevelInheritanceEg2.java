cd628d5372c6b5ec908e8466963c4444e7ade6cc
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-02-14 00:11:56 +0530

    Update MultiLevelInheritanceEg2.java

--- 13_Feb_Inheritance/MultiLevelInheritanceEg2.java
+++ 13_Feb_Inheritance/MultiLevelInheritanceEg2.java
@@ -1,18 +1,18 @@
 package inheritance;
- public class MultiLevelInheritanceEg2 {
+ public class MIChildEg2 {
 	int salary = 40000;
  }
-	class MIChildEg1 extends MultiLevelInheritanceEg2 {
+	class MIChildEg1 extends MIChildEg2 {
 		int bonous = 4000;
 		int annualsalary = salary+bonous;   // as = Annual Salary
 	}
 
-class MIChildEg2 extends MIChildEg1{
+class MultiLevelInheritanceEg2 extends MultiLevelInheritanceEg2 {
 	int da = 400;   //da = Dinner allowance
 	int totalsalary = annualsalary+da;
 
 	public static void main(String[] args) {
-		MIChildEg2 object = new MIChildEg2();
+		MultiLevelInheritanceEg2 object = new MultiLevelInheritanceEg2();
 		System.out.println("Salary is: "+ object.salary);
 		System.out.println("Annual Salary is: "+ object.annualsalary);
 		System.out.println("Total Salary is: "+ object.totalsalary);
