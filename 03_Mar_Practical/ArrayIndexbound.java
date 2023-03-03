8de92eb646c86451336d0a73ba5ee96788321b54
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-03-03 23:53:24 +0530

    Add files via upload

---
+++ 03_Mar_Practical/ArrayIndexbound.java
@@ -0,0 +1,8 @@
+package march_03;
+
+public class ArrayIndexbound {
+	public static void main(String[] args) {
+		int a[]= {2,5,4,8,9};
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}}}
